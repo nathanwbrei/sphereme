9ecf3bfe7e3b39ace90f657e1fac21b2802c596c
Katherine F <katfang>
2012-06-23 15:40:55 -0400

    android eclipse thing inside.

---
+++ src/com/theamrzone/android/sphereme/SpheremeActivity.java
@@ -0,0 +1,13 @@
+package com.theamrzone.android.sphereme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpheremeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
