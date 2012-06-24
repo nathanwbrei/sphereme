de4c2c318fa87ae4028dc539811dad13fe62278f
Katherine F <katfang>
2012-06-24 10:29:41 -0400

    SphereActivty ... has a lttle bit of functionality

--- src/com/theamrzone/android/sphereme/SpheremeActivity.java
+++ src/com/theamrzone/android/sphereme/SpheremeActivity.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class SpheremeActivity extends TapAndSensingActivity {
@@ -13,14 +14,14 @@ public class SpheremeActivity extends TapAndSensingActivity {
 	private TextBasedWorldView worldView;
 	private NoteDatabaseHelper dbHelper;
 	
+	private String newNoteContent;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	// DB
         dbHelper = NoteDatabaseHelper.getInstance(this);
-        
-        // saving a new message thingy
-        saveNewMessageFromEditor();
+        dbHelper.flush();
         
     	// view
         worldView = new TextBasedWorldView(this);
@@ -29,18 +30,17 @@ public class SpheremeActivity extends TapAndSensingActivity {
 	    
 	    // doing the tapping and the sensing thing
         super.onCreate(savedInstanceState, worldView);
+        
+        // saving a new message thingy
+        saveNewMessageFromEditor();
     }
     
     private void generateNoteViews() {
     	List<AbstractNote> notes = dbHelper.getNotes();
     	
-    	double[] rs = {1,1,1};  // does nothing for now
-    	double[] ts = {1,2,3};	// discrete buckets 1..30
-    	double[] zs = {5,5,7};	// discrete buckets 0..10
-    	
     	for (int i=0;i< notes.size();i++){
     		AbstractNote note = notes.get(i);
-    		note.setRTZ(rs[i], ts[i], zs[i]); 
+    		note.setRTZ(0, note.getT(), 3); 
     		NoteView noteView = new NoteView(this, note);
     		
     		worldView.addNoteView(noteView);
@@ -50,27 +50,30 @@ public class SpheremeActivity extends TapAndSensingActivity {
 	private void saveNewMessageFromEditor() {
 		// Get the message from the Editor intent
         Intent intent = getIntent();
-        String newContent = intent.getStringExtra(EditorActivity.NEW_NOTE);
-        
-        // save content if exists
-        if (newContent != null) {
-        	saveNewNote(newContent);
-        }
-        
-        // TODO generate new image for the note probably
+        newNoteContent = intent.getStringExtra(EditorActivity.NEW_NOTE);
 	}
 	
-	private void saveNewNote(String newContent) {
-		Note note = new Note(0, 0, 0, 0, 0, 0, 
+	private void saveNewNote(String newContent, double theta) {
+		Log.d("SpheremeActivty", "Creatng new content: " + newContent);
+		Note note = new Note(0, theta, 0, 0, 0, 0, 
 				Note.STRING, 
 				Note.stringToBitmap(newContent), // generate bitmap from string
 				Note.stringToByte(newContent));  // generate byte content from string 
-		note.save(dbHelper);
+		note.save(dbHelper); 
+		
+		NoteView noteView = new NoteView(this, note);
+		worldView.addNoteView(noteView);
 	}
 	
 	@Override
 	public void onSensorChanged(SensorInfo info) {
+		
 		worldView.theta = info.VisualColumn;
+		
+		if (newNoteContent != null) {
+			saveNewNote(newNoteContent, info.VisualColumn);
+			newNoteContent = null;
+		}
 		worldView.requestLayout();
 	}
 	
