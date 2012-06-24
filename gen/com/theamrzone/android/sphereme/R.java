8b8319bb59ddb67cf87a091be5c88e47b1d53233
Katherine F <katfang>
2012-06-24 12:26:14 -0400

    some extra stuff to make update easlier in the futre, but not actually used.

--- gen/com/theamrzone/android/sphereme/R.java
+++ gen/com/theamrzone/android/sphereme/R.java
@@ -19,28 +19,34 @@ public final class R {
         public static final int roundbutton=0x7f020002;
     }
     public static final class id {
-        public static final int action=0x7f06000f;
+        public static final int action=0x7f060011;
+        public static final int canvas_container=0x7f060006;
+        public static final int canvas_view=0x7f060007;
         public static final int direction=0x7f060000;
         public static final int edit_box=0x7f060003;
         public static final int editor_layout=0x7f060002;
-        public static final int event=0x7f060007;
+        public static final int event=0x7f060009;
         public static final int hello=0x7f060005;
-        public static final int max_accel=0x7f06000e;
-        public static final int max_x=0x7f06000b;
-        public static final int max_y=0x7f06000c;
-        public static final int max_z=0x7f06000d;
+        public static final int max_accel=0x7f060010;
+        public static final int max_x=0x7f06000d;
+        public static final int max_y=0x7f06000e;
+        public static final int max_z=0x7f06000f;
         public static final int save_button=0x7f060004;
-        public static final int tap_layout=0x7f060006;
-        public static final int time=0x7f060008;
+        public static final int tap_layout=0x7f060008;
+        public static final int time=0x7f06000a;
+        public static final int update_box=0x7f060013;
+        public static final int update_layout=0x7f060012;
         public static final int values=0x7f060001;
-        public static final int x=0x7f060009;
-        public static final int y=0x7f06000a;
+        public static final int x=0x7f06000b;
+        public static final int y=0x7f06000c;
     }
     public static final class layout {
         public static final int compass=0x7f030000;
         public static final int editor=0x7f030001;
         public static final int main=0x7f030002;
-        public static final int tap=0x7f030003;
+        public static final int pencanvas=0x7f030003;
+        public static final int tap=0x7f030004;
+        public static final int updater=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
