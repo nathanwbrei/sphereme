5e7785a1a8f1b1027e3d94856bc7d13db87e671e
Raheem Adam <raheem.adam@gmail.com>
2012-06-24 11:48:48 -0400

    Samsung pen classes

---
+++ src/com/theamrzone/android/sphereme/PenActivity.java
@@ -0,0 +1,32 @@
+package com.theamrzone.android.sphereme;
+
+import com.samsung.spensdk.SCanvasView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class PenActivity extends Activity  {
+    public final static String NEW_IMAGE = "com.theamrzone.android.sphereme.IMAGE";
+    
+	private SCanvasView msCanvas;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pencanvas);
+		
+		msCanvas = (SCanvasView) findViewById(R.id.canvas_view);
+	}
+	
+	public void acquireImage(View view) {
+		Intent intent = new Intent(this, SpheremeActivity.class);
+	    Bitmap b= msCanvas.getCanvasBitmap(true);
+	    intent.putExtra(NEW_IMAGE, b);
+	    startActivity(intent);
+	}
+	
+}
